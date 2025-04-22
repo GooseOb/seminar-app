15d634ac3e788a1e56649240798b6d2ac733f519
GooseOb <gooose.ob@gmail.com>
2025-04-22 02:09:49 +0200

    add registration forms

--- src/routes/login/+page.svelte
+++ src/routes/login/+page.svelte
@@ -1,24 +1,16 @@
 <script lang="ts">
 	import { enhance } from '$app/forms';
+	import Input from '$lib/form/input.svelte';
 
-	let { form } = $props();
-
-	console.log('Form data:', form);
+	const { form } = $props();
 </script>
 
 <div class="container">
 	<form method="POST" use:enhance>
 		<h2 class="header">Login</h2>
 
-		<div class="form-group">
-			<label for="email">Email</label>
-			<input type="email" name="email" required placeholder="Enter your email" />
-		</div>
-
-		<div class="form-group">
-			<label for="password">Password</label>
-			<input type="password" name="password" required placeholder="Enter your password" />
-		</div>
+		<Input type="email" name="email" label="Email" />
+		<Input type="password" name="password" label="Password" />
 
 		<button type="submit" class="btn">Log in</button>
 
@@ -30,69 +22,5 @@
 </div>
 
 <style>
-	.container {
-		font-size: min(4vw, 4vh);
-		/* max-width: 400px; */
-		padding: 1em;
-		gap: 1em;
-		/* background-color: var(--bg3-color); */
-		border-radius: 0.5em;
-		display: flex;
-		flex-direction: column;
-	}
-
-	form {
-		display: flex;
-		flex-direction: column;
-		gap: 1em;
-	}
-
-	h2 {
-		text-align: center;
-		margin-bottom: 1rem;
-		color: var(--fg-color);
-	}
-
-	.form-group {
-		display: flex;
-		flex-direction: column;
-		gap: 0.5rem;
-	}
-
-	label {
-		color: var(--fg-color);
-		font-size: 0.9em;
-		margin-bottom: 0.25em;
-	}
-
-	input {
-		font-size: 1em;
-		padding: 0.5em;
-		border-radius: 0.5em;
-		background-color: var(--bg2-color);
-		color: var(--fg-color);
-	}
-
-	input:focus {
-		outline: 2px solid var(--primary-color);
-	}
-
-	.btn {
-		font-size: 1em;
-		border-radius: 0.5em;
-	}
-
-	.error {
-		color: var(--danger-color);
-		text-align: center;
-	}
-
-	a {
-		text-decoration: underline;
-		text-align: center;
-		transition: color 0.2s;
-	}
-	a:hover {
-		color: var(--primary-color);
-	}
+	@import '$lib/form/styles.css';
 </style>
---
+++ src/routes/register/teacher/+page.svelte
@@ -0,0 +1,28 @@
+<script lang="ts">
+	import { enhance } from '$app/forms';
+	import Input from '$lib/form/input.svelte';
+
+	const { form } = $props();
+</script>
+
+<div class="container">
+	<h2 class="header">Sign up</h2>
+
+	<form method="POST" use:enhance>
+		<Input type="text" name="firstname" label="Firstname" />
+		<Input type="text" name="lastname" label="Lastname" />
+		<hr />
+		<Input type="email" name="email" label="Email" />
+		<Input type="password" name="password" label="Password" />
+
+		<button type="submit" class="btn">Sign up</button>
+
+		{#if form?.message}
+			<div class="error">{form.message}</div>
+		{/if}
+	</form>
+</div>
+
+<style>
+	@import '$lib/form/styles.css';
+</style>
