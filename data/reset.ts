a9746a5699c95cfabaf92c500df6db165b4889ca
GooseOb <gooose.ob@gmail.com>
2025-06-22 04:27:03 +0200

    remove setting db url in reset

--- data/reset.ts
+++ data/reset.ts
@@ -1,8 +1,6 @@
-import { db, setDatabaseUrl } from '../src/lib/server/db';
+import { db } from '../src/lib/server/db';
 import * as schema from '../src/lib/server/db/schema';
 import { reset } from 'drizzle-seed';
-
-setDatabaseUrl(process.env.DATABASE_URL!);
 
 await reset(db(), schema);
 
