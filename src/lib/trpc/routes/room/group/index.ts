c2fec844e3c296ad2b8c926a7c81531c1437bbf4
GooseOb <gooose.ob@gmail.com>
2025-06-22 17:27:33 +0200

    remove trpc middleware duplicate

--- src/lib/trpc/routes/room/group/index.ts
+++ src/lib/trpc/routes/room/group/index.ts
@@ -1,7 +1,7 @@
 import { removeMemberFromRoom } from '$lib/server/db/queries/room/removeMember';
+import { groupOwnerProcedure } from '$lib/trpc/middleware/groupOwner';
 import { t } from '$lib/trpc/t';
 import { z } from 'zod';
-import { groupOwnerProcedure } from './middleware';
 
 export const groupRouter = t.router({
 	removeStudent: groupOwnerProcedure
