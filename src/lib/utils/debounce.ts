a578edcdbb148330ec2448d5a406305e6bd2cb08
GooseOb <gooose.ob@gmail.com>
2025-09-22 00:32:34 +0200

    remove unused code

--- src/lib/utils/debounce.ts
+++ src/lib/utils/debounce.ts
@@ -1,32 +1,11 @@
-export const debounce = <
-	TArgs extends any[],
-	T extends (...args: TArgs) => any
->(
-	fn: T,
+export const debounce = <TArgs extends any[]>(
+	fn: (...args: TArgs) => any,
 	ms: number
-): ((...args: TArgs) => void) => {
+) => {
 	let timeout: NodeJS.Timeout;
 
 	return (...args: TArgs) => {
 		clearTimeout(timeout);
 		timeout = setTimeout(() => fn(...args), ms);
-	};
-};
-
-export const throttle = <
-	TArgs extends any[],
-	T extends (...args: TArgs) => any
->(
-	fn: T,
-	ms: number
-): ((...args: TArgs) => void) => {
-	let timerFlag: null | number = null;
-	return (...args: TArgs) => {
-		if (timerFlag === null) {
-			timerFlag = window.setTimeout(() => {
-				fn(...args);
-				timerFlag = null;
-			}, ms);
-		}
 	};
 };
